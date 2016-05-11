558166acab903b25faebc490e17b8b739c6c8408
Joe Fleming <joe.fleming@gmail.com>
2016-05-11 16:17:51 -0700

    properly join the test answers to the questions

--- map_questions.js
+++ map_questions.js
@@ -36,23 +36,17 @@ fs.createReadStream(path.join(__dirname, 'data', 'question_data.csv'))
   fs.createReadStream(path.join(__dirname, 'data', 'test_items.csv'))
   .pipe(testParser)
   .on('data', function (line) {
-    var mapped = {
-      id: line.id,
-      text: line.text,
-      option_1: line.option_1,
-      option_2: line.option_2,
-      option_3: line.option_3,
-      option_4: line.option_4,
-      option_1_count: 0,
-      option_2_count: 0,
-      option_3_count: 0,
-      option_4_count: 0,
-      option_correct: line.option_correct,
-      Type: 'T',
-      Keywords: 'test',
-    }
-    if (mapped.id.match(/^q[1-9]/)) {
-      db('questions').push(mapped);
+    if (line.id.match(/^q[1-9]/)) {
+      db('questions')
+      .chain()
+      .find({ id: line.id })
+      .thru(function (q) {
+        if (!q) return;
+        q.option_correct = line.option_correct;
+        q.Keywords += '; test';
+        return q;
+      })
+      .value();
     }
   })
   .on('end', function () {
